60883379c8958511d5c61ec0ef165c983b155a7c
Nyah Anderson <nkarpaiya@yahoo.co.uk>
2021-03-11 18:29:57 +0000

    test2

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+var cacheName = 'webstore-v1';
+var cacheFiles = [
+'index.html',
+'webstore.webmanifest',
+'art.png',
+'drama.png',
+'english.png',
+'french.png',
+'geography.png',
+'history.png',
+'maths.png',
+'music.png',
+'pe.png',
+'science.png',
+'schoolicon.png'
+];
+
+self.addEventListener('install'
+, (e) => {
+console.log('[Service Worker] Install');
+e.waitUntil(
+caches.open(cacheName).then((cache) => {
+console.log('[Service Worker] Caching all the files');
+return cache.addAll(cacheFiles);
+})
+);
+});
+
+self.addEventListener('fetch'
+, function (e) {
+e.respondWith(
+// check if the cache has the file
+caches.match(e.request).then(function (r) {
+console.log('[Service Worker] Fetching resource: '
++ e.request.url);
+// 'r' is the matching file if it exists in the cache
+return r
+})
+);
+});
+
+self.addEventListener('fetch'
+, function (e) {
+e.respondWith(
+caches.match(e.request).then(function (r) {
+// Download the file if it is not in the cache,
+return r || fetch(e.request).then(function (response) {
+// add the new file to cache
+return caches.open(cacheName).then(function (cache) {
+cache.put(e.request, response.clone());
+return response;
+});
+});
+})
+);
+});
+
